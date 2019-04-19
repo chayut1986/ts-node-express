60e5e9743418e8eb52d9235ae4b99ee96e5d5ccc
chayut1986 <chayut1986@gmail.com>
2019-04-19 11:31:33 +0700

    add models department and routes

---
+++ src/models/department.ts
@@ -0,0 +1,27 @@
+import * as Knex from 'knex';
+
+export class DepartmentModel {
+
+    getDepartmentList(db: Knex) {
+        return db('departments')
+            .orderBy('department_id', 'ASC');
+
+    }
+
+    addDepartment(db: Knex, data: any) {
+        return db('departments')
+            .insert(data);
+    }
+
+    updateDepartment(db: Knex, departmentId: any, depertmentName: any) {
+        return db('department')
+            .where('deparitment_id', departmentId)
+            .update({ department_name: depertmentName });
+    }
+
+
+    deleteDepartment(db: Knex, departmentId: any) {
+        return db('department')
+            .del();
+    }
+}
---
+++ src/routes/departments.ts
@@ -0,0 +1,29 @@
+
+
+import * as express from 'express';
+import { Router, Request, Response } from 'express';
+import { DepartmentModel } from '../models/department';
+
+import * as HttpStatus from 'http-status-codes';
+
+const departmentModel = new DepartmentModel();
+
+const router: Router = Router();
+
+router.get('/', async (req: Request, res: Response) => {
+
+    let db = req.db;
+
+    try {
+        let rs = await departmentModel.getDepartmentList(db);
+        res.send({ ok: true, rows: rs });
+    } catch (error) {
+        res.send({ ok: false, error: error.message });
+    }
+
+
+});
+
+
+
+export default router;
